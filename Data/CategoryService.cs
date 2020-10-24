2eff064b2b5c426fbe197f5698144ec0b51c93d9
ongthai <ongthai2803@gmail.com>
2020-10-25 00:20:13 +1300

    Add Overlap Logic for Create Category, Add Update and Delete for Category

--- Data/CategoryService.cs
+++ Data/CategoryService.cs
@@ -15,11 +15,44 @@ namespace CRUD_Blazor.Data
             this._dbContext = dbContext;
         }
 
-        public List<Category> GetCategory()
+        public List<Category> GetCategories()
         {
             var categories = _dbContext.Categories.ToList();
 
             return categories;
+        }
+
+        public Category GetCategoryById(int id)
+        {
+            return _dbContext.Categories.FirstOrDefault(x => x.Id.Equals(id));
+        }
+
+        public string Create(Category category)
+        {
+            var categories = _dbContext.Categories.ToList();
+
+            var isOverlap = categories.Any(x => x.MinWeight <= category.MinWeight && category.MaxWeight <= x.MaxWeight);
+
+            if (!isOverlap)
+            {
+                _dbContext.Add(category);
+                _dbContext.SaveChanges();
+
+                return "Successful";
+            }
+
+            return "New item's range is overlap or not covered the gap of current ranges.";
+        }
+
+        public void Update(Category category)
+        {
+            _dbContext.Update(category);
+            _dbContext.SaveChanges();
+        }
+
+        public void Delete()
+        {
+
         }
     }
 }
