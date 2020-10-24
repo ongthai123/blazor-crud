0f98ebb931d4918f4264be7a11920e8f66ba9678
ongthai <ongthai2803@gmail.com>
2020-10-25 00:25:04 +1300

    Commit side files

--- Migrations/20201024024203_UpdateCategory.Designer.cs
+++ Migrations/20201024024203_UpdateCategory.Designer.cs
@@ -4,14 +4,16 @@ using CRUD_Blazor.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CRUD_Blazor.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201024024203_UpdateCategory")]
+    partial class UpdateCategory
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
