326ab34615d77a5d99b93050df48a15c42ff53d6
ongthai <ongthai2803@gmail.com>
2020-10-25 00:22:34 +1300

    Seed Data

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -19,9 +19,9 @@ namespace CRUD_Blazor.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Category>().HasData(
-                new Category { Id = 1, Name = "Light", WeightMin = 0, WeightMax = 500, IconURL = "" },
-                new Category { Id = 2, Name = "Medium", WeightMin = 500, WeightMax = 2500, IconURL = "" },
-                new Category { Id = 3, Name = "Heavy", WeightMin = 2500, WeightMax = 999999, IconURL = "" }
+                new Category { Id = 1, Name = "Light", MinWeight = 0, MaxWeight = 500, IconURL = "https://www.flaticon.com/svg/static/icons/svg/1085/1085961.svg" },
+                new Category { Id = 2, Name = "Medium", MinWeight = 500, MaxWeight = 2500, IconURL = "https://www.flaticon.com/svg/static/icons/svg/3662/3662796.svg" },
+                new Category { Id = 3, Name = "Heavy", MinWeight = 2500, MaxWeight = 999999, IconURL = "https://www.flaticon.com/svg/static/icons/svg/3662/3662736.svg" }
             );
 
             modelBuilder.Entity<Vehicle>().ToTable("Vehicle");
