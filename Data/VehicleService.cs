9c06bf3507e647b99e87ab86c66a7fbe28ac691c
ongthai <ongthai2803@gmail.com>
2020-10-26 22:59:03 +1300

    Add simple validation, and sorting

--- Data/VehicleService.cs
+++ Data/VehicleService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,9 +15,40 @@ namespace CRUD_Blazor.Data
             this._dbContext = dbContext;
         }
 
-        public List<Vehicle> GetVehicles()
+        public List<Vehicle> GetVehicles(OrderBy order, bool isAscending)
         {
-            return _dbContext.Vehicles.ToList();
+            if (isAscending)
+            {
+                switch (order)
+                {
+                    case OrderBy.Owner:
+                        return _dbContext.Vehicles.AsNoTracking().OrderBy(x => x.Owner).ToList();
+                    case OrderBy.Manufacturer:
+                        return _dbContext.Vehicles.AsNoTracking().OrderBy(x => x.Manufacturer).ToList();
+                    case OrderBy.ManufacturingYear:
+                        return _dbContext.Vehicles.AsNoTracking().OrderBy(x => x.ManufacturingYear).ToList();
+                    case OrderBy.Weight:
+                        return _dbContext.Vehicles.AsNoTracking().OrderBy(x => x.Weight).ToList();
+                    default:
+                        return _dbContext.Vehicles.AsNoTracking().OrderBy(x => x.Owner).ToList();
+                }
+            }
+            else
+            {
+                switch (order)
+                {
+                    case OrderBy.Owner:
+                        return _dbContext.Vehicles.AsNoTracking().OrderByDescending(x => x.Owner).ToList();
+                    case OrderBy.Manufacturer:
+                        return _dbContext.Vehicles.AsNoTracking().OrderByDescending(x => x.Manufacturer).ToList();
+                    case OrderBy.ManufacturingYear:
+                        return _dbContext.Vehicles.AsNoTracking().OrderByDescending(x => x.ManufacturingYear).ToList();
+                    case OrderBy.Weight:
+                        return _dbContext.Vehicles.AsNoTracking().OrderByDescending(x => x.Weight).ToList();
+                    default:
+                        return _dbContext.Vehicles.AsNoTracking().OrderByDescending(x => x.Owner).ToList();
+                }
+            }
         }
 
         public Vehicle GetVehicleById(int id)
@@ -26,6 +58,11 @@ namespace CRUD_Blazor.Data
 
         public string Create(Vehicle vehicle)
         {
+            if(vehicle.Owner == null || vehicle.ManufacturingYear == null || vehicle.Weight <= 0)
+            {
+                return "Please fill up the fields carefully.";
+            }
+
             _dbContext.Vehicles.Add(vehicle);
             _dbContext.SaveChanges();
 
@@ -34,6 +71,11 @@ namespace CRUD_Blazor.Data
 
         public string Update(Vehicle vehicle)
         {
+            if (vehicle.Owner == null || vehicle.ManufacturingYear == null || vehicle.Weight <= 0)
+            {
+                return "Please fill up the fields carefully.";
+            }
+
             _dbContext.Vehicles.Update(vehicle);
             _dbContext.SaveChanges();
 
