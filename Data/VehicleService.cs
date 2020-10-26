a30a7ae45d24375f246972d8ccf7ec8c39a4fa09
ongthai <ongthai2803@gmail.com>
2020-10-26 15:26:17 +1300

    Add logic

--- Data/VehicleService.cs
+++ Data/VehicleService.cs
@@ -7,9 +7,43 @@ namespace CRUD_Blazor.Data
 {
     public class VehicleService
     {
-        //public Task<Vehicle[]> Create()
-        //{
+        public ApplicationDbContext _dbContext;
 
-        //}
+        public VehicleService(ApplicationDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
+        public List<Vehicle> GetVehicles()
+        {
+            return _dbContext.Vehicles.ToList();
+        }
+
+        public Vehicle GetVehicleById(int id)
+        {
+            return _dbContext.Vehicles.FirstOrDefault(x => x.Id.Equals(id));
+        }
+
+        public string Create(Vehicle vehicle)
+        {
+            _dbContext.Vehicles.Add(vehicle);
+            _dbContext.SaveChanges();
+
+            return "";
+        }
+
+        public string Update(Vehicle vehicle)
+        {
+            _dbContext.Vehicles.Update(vehicle);
+            _dbContext.SaveChanges();
+
+            return "";
+        }
+
+        public void Delete(Vehicle vehicle)
+        {
+            _dbContext.Vehicles.Remove(vehicle);
+            _dbContext.SaveChanges();
+        }
     }
 }
